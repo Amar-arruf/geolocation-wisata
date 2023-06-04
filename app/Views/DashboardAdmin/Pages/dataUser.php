29b3c1f25ebea3d6f7bfb38cb34b90409373d14c
Amar Maruf <amarmaruf2403@gmail.com>
2023-06-04 15:11:58 +0700

    memperbaiki host

--- app/Views/DashboardAdmin/Pages/dataUser.php
+++ app/Views/DashboardAdmin/Pages/dataUser.php
@@ -100,7 +100,7 @@
         confirmButtonText: 'ya, hapus itu!'
       }).then((result) => {
         if (result.isConfirmed) {
-          window.location.href = `https://geolocationwisata.com/admin/userdata/${getID}/hapus`;
+          window.location.href = `http://localhost:8080/admin/userdata/${getID}/hapus`;
         }
       })
     })
@@ -121,7 +121,7 @@
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          window.location.href = `https://geolocationwisata.com/admin/userdata/${dapatID}/edit`
+          window.location.href = `http://localhost:8080/admin/userdata/${dapatID}/edit`
         } else if (result.isDenied) {
           Swal.fire('Perubahan tidak tersimpan!', '', 'info')
         }
